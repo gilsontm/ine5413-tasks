967983235a1abc8adab1b35518f9bd324234fb1d
Gilson <gilson.t.magro@gmail.com>
2020-06-06 23:08:07 -0300

    Questão 1: representação

--- graph.py
+++ graph.py
@@ -1,14 +1,94 @@
+import sys
 
-class Graph:
-    def __init__(self, n, vertices, edges):
-        pass
+class Grafo:
+    """
+        Classe que representa um grafo ponderado não-dirigido.
 
-    @staticmethod
-    def build_graph(filename):
+        Utiliza tanto a representação de matriz de adjacência,
+        quanto a representação de vetor de adjacência.
+
+        Desse modo, o grafo ocupa mais espaço de armazenamento,
+        mas todas as operações são O(1).
+
+        IMPORTANTE: os vértices no arquivo de entrada devem ser
+        indexados em 1, enquanto as estruturas internas da classe
+        são indexadas em 0.
+    """
+
+    def __init__(self, filename):
+        """
+            Construtor da classe, recebe como parâmetro
+            o nome do arquivo de entrada que contém as
+            informações sobre o grafo.
+        """
+        self._labels = []
+        self._INFINITY = sys.maxsize
         with open(filename, "r") as file:
+            n = int(file.readline().split()[-1])
+            self._nVertices = n
+            for _ in range(n):
+                self._labels.append(file.readline().split()[-1])
 
-            n = file.read().split()[-1]
+            # Constrói uma matriz n por n
+            self._matrix = [[self._INFINITY] * n for _ in range(n)]
+            # Constrói uma lista (tamanho n) de listas vazias
+            self._vector = [[] for _ in range(n)]
 
+            # Pula a linha que contém '*edges'
+            file.readline()
+            # Lê todas as linhas subsequentes
+            edges = file.readlines()
+            self._nEdges = len(edges)
+            for edge in edges:
+                a, b, weight = edge.split()
+                a = int(a)-1; b = int(b)-1; weight = float(weight)
+                self._matrix[a][b] = self._matrix[b][a] = weight
+                self._vector[a].append((b+1, weight))
+                self._vector[b].append((a+1, weight))
+
+    def qtdVertices(self):
+        """ Retorna o número de vértices """
+        return self._nVertices
+    
+    def qtdArestas(self):
+        """ Retorna o número de arestas """
+        return self._nEdges
+
+    def grau(self, v):
+        """ Retorna o número de vizinhos do vértice v """
+        return len(self._vector[v-1])
+
+    def rotulo(self, v):
+        """ Retorna o rótulo (label) do vértice v """
+        return self._labels[v-1]
+
+    def vizinhos(self, v):
+        """
+            Retorna uma lista com os vizinhos do vértice v
+
+            IMPORTANTE: como o grafo é ponderado, a lista
+            retornada contém tuplas (vértice, peso).
+
+            IMPORTANTE: os índices dos vértices nas tuplas
+            (vértice, peso) são indexados em 1.
+        """
+        return self._vector[v-1]
+
+    def haAresta(self, u, v):
+        """ 
+            Retorna verdadeiro caso haja uma aresta entre 
+            os vértices u e v, e falso caso contrário
+        """
+        return self._matrix[u-1][v-1] != self._INFINITY
+
+    def peso(self, u, v):
+        """ 
+            Retorna o peso da aresta u-v, se existir;
+            caso contrário, retorna infinito positivo.
+        """
+        return self._matrix[u-1][v-1]
 
 if __name__ == "__main__":
-    Graph.build_graph(input())
+    # No terminal, execute:
+    # python graph.py ARQUIVO_DE_ENTRADA
+    grafo = Grafo(sys.argv[1])
